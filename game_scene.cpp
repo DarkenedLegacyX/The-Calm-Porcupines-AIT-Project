6010c27e955cd056b174e633b99f57631974b758
DarkenedLegacyX <7092@ait.nsw.edu.au>
2020-05-26 19:24:18 +1000

    Initial commit

---
+++ game_scene.cpp
@@ -0,0 +1,34 @@
+#include "game_scene.h"
+#include "player.h"
+
+Game_Scene::Game_Scene()
+	: Scene("Game")
+{
+	/*
+	Game_Object* dino = new Dino("Dino");
+	_game_objects[dino->id()] = dino;
+
+	Game_Object* dino_walking = new Walking_Dino("Dino.Walking");
+	_game_objects[dino_walking->id()] = dino_walking;
+
+	Game_Object* player = new Player("Player");
+	_game_objects[player->id()] = player;
+
+	Game_Object* portal_entry = new Portal_Entry();
+	_game_objects[portal_entry->id()] = portal_entry;
+
+	Game_Object* portal_exit = new Portal_Exit();
+	_game_objects[portal_exit->id()] = portal_exit;
+	*/
+
+	add_game_object(new Player("Player"));
+
+}
+
+Game_Scene::~Game_Scene()
+{
+}
+
+void Game_Scene::update(SDL_Window*)
+{
+}
