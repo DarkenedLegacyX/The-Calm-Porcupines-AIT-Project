579e4d53e869a031fe0a80f898572089bcfe8929
DarkenedLegacyX <7092@ait.nsw.edu.au>
2020-06-16 17:31:41 +1000

    Sound attcks and enemy2

--- menu_background.cpp
+++ menu_background.cpp
@@ -7,6 +7,7 @@ Menu_background::Menu_background(std::string id) : Game_Object(id, "Texture.Menu
 	_width = 800;
 	_height = 800;
 
+
 }
 
 Menu_background::~Menu_background()
@@ -15,4 +16,21 @@ Menu_background::~Menu_background()
 
 void Menu_background::simulate_AI(Uint32, Assets* , Input*, Scene* ) 
 {
+}
+
+void Menu_background::render(Uint32, Assets* assets, SDL_Renderer* renderer, Configuration* config)
+{
+
+
+		SDL_Color text_color;
+		text_color.r = 255;
+		text_color.g = 0;
+		text_color.b = 0;
+		text_color.a = 0;
+
+		Text id(renderer, _id.c_str(), text_color, "ID.Text");
+
+
+		id.render(renderer, _translation);
+	
 }
